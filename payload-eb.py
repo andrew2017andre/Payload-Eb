53b9d46c17a7aec576b0f205ad3cca6b2256699d
josifkhan <42940742+josifkhan@users.noreply.github.com>
2019-10-14 11:16:52 +0600

    Update payload-eb.py

--- payload-eb.py
+++ payload-eb.py
@@ -1,32 +1,35 @@
 import requests,sys,termcolor
 from termcolor import colored
+import os
 
+os.system("clear")
 print(colored('============WELCOME TO BANgLADESH============','green'))
 print(colored('               ===============','green'))
 print(colored('                  BANgLADESH','green'))
 print(colored('                     V1.O', 'green'))
-print(colored("\n URL WITH http/https:", "yellow"))
+print(colored("\n URL WITHOUT http/https:", "yellow"))
 inp=input(colored("Target URL: ", "red"))
-x=input(colored("\n (1) Get Sources:\n (2) Get Response \n (3) Get Cookies (If Available)\n >> ", "blue"))
+x=input(colored("\n (1) Get Sources:\n (2) Get Response \n (3) Get Cookies (If Available)\n >> ", "cyan"))
 def source():
-	s=requests.get(inp)
+	s=requests.get("http://"+inp)
 	sources=open('./LastUrl-Source.txt','w')
-	s.encoding='ISO-8859-1'
+
 	if x=="1":
+		s.encoding='ISO-8859-1'
 		print(sources.write(str(s.text)))
 		print(colored('Successfully Cracked ! \n Check LastUrl-Source.txt ', 'green'))
 source()
 
 def get():
-	gett=requests.get(inp)
+	gett=requests.get("http://"+inp)
 	if x=="2":
 		print(colored("Respond code: ", "red"),gett.status_code)
 get()
 def cookie():
-	k=requests.get(inp)
+	k=requests.get("http://"+inp)
 	cookied=open('./Cookies.txt', 'w')
-	k.encoding='ISO-8859-1'
 	if x=="3":
+		k.encoding='ISO-8859-1'
 		print(cookied.write(str(k.cookies.get_dict())))
 		print(colored("Successfully Cracked Cookies ! \n Check Cookies.txt ", "green"))
 	if x=="exit":
